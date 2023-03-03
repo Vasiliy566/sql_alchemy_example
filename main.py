871d178f0f75dcee3b47f89b56277eba865dd992
isaev-vs <isaev-vs@zvuk.com>
2023-03-03 20:08:16 +0300

    Add BaseDb

--- main.py
+++ main.py
@@ -1,46 +1,58 @@
 from datetime import datetime
 
-from db.base import Session
+from db.base import Session, get_session
+from db.base_db import DbBase
 from db.models import User
 
 
-def add_user(name: str, birthday: datetime, work_quality: int, password: str) -> None:
-    user = User(name=name, birthday=birthday, work_quality=work_quality, password=password)
-    session = Session()
-    session.add(user)
-    session.commit()
-    session.close()
+# def add_user(session: Session, name: str, birthday: datetime, work_quality: int, password: str) -> None:
+#     user = User(name=name, birthday=birthday, work_quality=work_quality, password=password)
+#     session.add(user)
+#     session.commit()
+#
+#
+# def get_user(session: Session, user_id: int) -> User | None:
+#     return session.query(User).filter_by(id=user_id).first()
+#
+#
+# def delete_user(session: Session, user_id: int) -> None:
+#     user = session.query(User).filter_by(id=user_id).first()
+#     session.delete(user)
+#     session.commit()
+#
+#
+# def change_password(session: Session, user_id: int, new_password: str) -> User:
+#     user = session.query(User).filter_by(id=user_id).first()
+#     user.password = new_password
+#     session.add(user)
+#     session.commit()
+#     return get_user(session, user_id)
 
 
-def get_user(user_id: int) -> User | None:
-    session = Session()
-    user = session.query(User).filter_by(id=user_id).first()
-    session.close()
-    return user
+class DbUsers(DbBase):
+    data_model = User
+
+    def add(self, data: dict):
+        with get_session() as session:
+            to_add = self.data_model(**data)
+            session.add(to_add)
+            session.commit()
+
+    def get(self, filtered_by: dict):
+        with get_session() as session:
+            return session.query(self.data_model).filter_by(**filtered_by).first()
 
 
-def delete_user(user_id: int):
-    session = Session()
-    user = session.query(User).filter_by(id=user_id).first()
-    session.delete(user)
-    session.commit()
-    session.close()
+if __name__ == "__main__":
+    db = DbUsers()
+    d = {"name": "Vasya", "birthday": datetime.now(), "work_quality": 0, "password": "Vasya1"}
+    db.add(d)
 
+    u = db.get({"name": "Vasya"})
+    print(u.id)
 
-def change_password(user_id: int, new_password: str) -> User:
-    session = Session()
-    user = session.query(User).filter_by(id=user_id).first()
-    user.password = new_password
-    session.add(user)
-    session.commit()
-    session.close()
-    return get_user(user_id)
+    u = db.get({"work_quality": "0"})
+    print(u.id)
 
-
-u = get_user(2)
-print(u)
-
-change_password(2, "123456")
-u = get_user(2)
-print(u)
-
+    u = db.get({"password": "Vasya1"})
+    print(u.id)
